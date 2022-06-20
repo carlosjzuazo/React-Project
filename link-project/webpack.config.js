9169d366acfbddea658a2f2b05e65b472a884160
nanot <[carlitosjzuazo@gmail.com]>
2022-06-20 21:42:17 +0200

    reinstalamos módulos

--- link-project/webpack.config.js
+++ link-project/webpack.config.js
@@ -1,22 +1,20 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-module.exports = {
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      // eslint-disable-next-line n/no-path-concat
-      template: __dirname + '/src/index.html',
-      filename: 'index.html',
-      inject: 'body',
-    }),
+export const mode = 'development';
+export const module = {
+  rules: [
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: ['babel-loader'],
+    },
   ],
 };
+export const plugins = [
+  new HtmlWebpackPlugin({
+    // eslint-disable-next-line n/no-path-concat
+    template: __dirname + '/src/index.html',
+    filename: 'index.html',
+    inject: 'body',
+  }),
+];
